bb2a0dc77afcc4941ce9b3ef3cd39e989bed5368
Modante <34290616+modante@users.noreply.github.com>
2024-11-12 01:00:10 +0100

    Update RaspiLoopStation.sh

--- RaspiLoopStation.sh
+++ RaspiLoopStation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 killall fluidsynth
-killall jackd
 jack_control stop
-sleep 1
-#jackd -dalsa -r48000 -p512 -Xraw -D -Chw:H5 -Phw:H5 &
-read -t 2 -p "I am going to wait for 2 seconds only ..."
+killall jackd
+sleep 2
+jackd -d alsa -r 48000 -p 512 -n 2 -X raw -D -C hw:H5 -P hw:H5 &
+#read -t 2 -p "I am going to wait for 2 seconds only ..."
 cd ~/shared/RaspiLoopStation/
 until python ./RaspiLoopStation.py
 do
